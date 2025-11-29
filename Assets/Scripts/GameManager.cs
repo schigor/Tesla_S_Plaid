22e186714f2adc298acfd612e544c3449b8a4620
JayKobeS <jk311153@student.polsl.pl>
2025-11-29 19:08:12 +0100

    gamemanger

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using TMPro;
+
+public class MenuController : MonoBehaviour
+{
+    // Miejsce, gdzie przypiszemy nasz tekst w Unity
+    public TextMeshProUGUI myText; 
+
+    // Ta funkcja musi być PUBLICZNA, żeby przycisk ją widział
+    public void ChangeMessage()
+    {
+        myText.text = "Witaj w Unity!";
+        Debug.Log("Przycisk został kliknięty!");
+    }
+    
+    public void QuitGame()
+    {
+        // Ta komenda zamyka zbudowaną aplikację
+        Application.Quit();
+
+        // To wyświetli komunikat w konsoli Unity, żebyś wiedział, że przycisk działa
+        // (ponieważ Application.Quit nie zadziała w samym edytorze)
+        Debug.Log("Gra została zamknięta! (Działa tylko w buildzie)");
+        
+        // Opcjonalnie: Ten kod poniżej wymusi zatrzymanie gry w Edytorze Unity
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
